da9aed2a0ab29163d07e0df64ebd80bf9363cf9e
Shubham Pratap Singh <singh.gaya101@gmail.com>
2024-11-02 21:18:06 +0530

    basic setup for socket

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,84 @@
-import React from "react";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 
-const Account = () => {
-  return <div className="py-6">Account</div>;
-};
+const socket = io.connect("http://localhost:3001");
 
-export default Account;
+function Dashboard() {
+  // Room State
+  const [room, setRoom] = useState("");
+  const [isActiveRoom, setIsActiveRoom] = useState(false);
+
+  // Messages States
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+      setIsActiveRoom(true);
+    }
+  };
+
+  const handleMessageChange = (event) => {
+    const newMessage = event.target.value;
+    setMessage(newMessage);
+
+    if (newMessage.trim() !== "") {
+      socket.emit("send_message", { message: newMessage, room });
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+
+    // Clean up on unmount
+    return () => {
+      socket.off("receive_message");
+    };
+  }, [socket]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      {!isActiveRoom ? (
+        <div className="flex flex-col mb-4 w-full max-w-md">
+          <input
+            type="text"
+            placeholder="Enter Room Number..."
+            className="p-2 border border-gray-300 rounded mb-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button
+            onClick={joinRoom}
+            className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition"
+          >
+            Create/Join Room
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h1 className="text-2xl font-bold mb-6">{room}</h1>
+        </div>
+      )}
+
+      <div className="flex flex-col mb-4 w-full max-w-md">
+        <input
+          type="text"
+          placeholder="Enter Your Message..."
+          className="p-2 border border-gray-300 rounded mb-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={message}
+          onChange={handleMessageChange}
+        />
+      </div>
+
+      <h2 className="text-xl font-semibold mt-6">Message:</h2>
+      <p className="text-yellow-800">{message}</p>
+      <p className="text-gray-600">{messageReceived}</p>
+    </div>
+  );
+}
+
+export default Dashboard;
